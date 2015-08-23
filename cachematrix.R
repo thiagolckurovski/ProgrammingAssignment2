5283373298c12af349d8cb14c3afd4255e0f31f4
Thiago Kurovski <thiagolckurovski@gmail.com>
2015-08-23 09:20:59 -0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -16,14 +16,30 @@
 ## - setinv sets the cached inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+  # Initialize cached inverse with NULL
   i <- NULL
+
+  # Set the new matrix, initialize cached
+  # inverse with NULL
+  # Need <<- to set internal scope
   set <- function(y) {
     x <<- y
     i <<- NULL
   }
+
+  # Return matrix
   get <- function() x
+  
+  # Internal function: set cached matrix
+  # Need <<- to set internal scope
   setinv <- function(inv) i <<- inv
+  
+  # Internal function: get cached matrix
   getinv <- function() i
+  
+  # Return list of functions required to
+  # access internal cached data,
+  # plus set and get interfaces
   list(set = set, get = get,
        setinv = setinv,
        getinv = getinv)
@@ -37,11 +53,19 @@ makeCacheMatrix <- function(x = matrix()) {
 ## stores it in the cache and returns the result
 
 cacheSolve <- function(x, ...) {
+  # Obtain cached data
   i <- x$getinv()
+  
+  # If the cached data is not NULL
+  # return it
   if(!is.null(i)) {
     message("getting cached data")
     return(i)
   }
+  
+  # Otherwise generate a cached
+  # inverse using solve(), store
+  # it and return it
   data <- x$get()
   i <- solve(data, ...)
   x$setinv(i)
